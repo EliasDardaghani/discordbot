7d24b0db2aaf478dc0d703c92520ea128d68e92d
haha150 <alisymeri@hotmail.com>
2023-04-09 02:38:42 +0200

    Added .env and config Added support for multiple servers

--- deals_scraper.py
+++ deals_scraper.py
@@ -2,20 +2,22 @@ from discord.ext import tasks, commands
 import logging
 
 class DealsScraperCog(commands.Cog):
-    def __init__(self, bot, channel):
+    def __init__(self, bot, channels):
         self.logger = logging.getLogger('discord')
         self.bot = bot
-        self.channel = channel
+        self.channels = channels
         self.scrape.start()
 
     def cog_unload(self):
         self.logger.info('DealsScraperCog unloading')
-        self.printer.cancel()
+        self.scrape.cancel()
 
     @tasks.loop(minutes=1)
     async def scrape(self):
         self.logger.info('DealsScraperCog started')
-        await self.channel.send('Testing background task')
+        # scrape websites for deals below
+        # for channel in self.channels:
+            # await self.channel.send('NEW DEAL')
 
     @scrape.before_loop
     async def before_printer(self):
--- main.py
+++ main.py
@@ -1,31 +1,64 @@
 import discord
 import logging
-from deals import DealsScraperCog
+from deals_scraper import DealsScraperCog
 from discord.ext import commands
+import configparser
+import os
+from dotenv import load_dotenv
+
+CONFIG = 'config.ini'
+CONFIG_DISCORD = 'Discord'
+CONFIG_DISCORD_SERVERS = 'servers'
+CONFIG_DISCORD_CHANNELS = 'channels'
+LOG_FILE = 'discord.log'
+LOGGER = 'discord'
+TOKEN = 'TOKEN'
 
 # https://discordpy.readthedocs.io/en/stable/api.html
 
 class DealsBot(commands.Bot):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, servers, channels, logger, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.logger = logging.getLogger('discord')
+        self.servers = servers
+        self.channels = channels
+        print(self.servers)
+        self.logger = logger
+        self.allowed_channels = []
 
     async def on_ready(self):
         self.logger.info(f'Logged in as {self.user}')
         for guild in self.guilds:
-            self.logger.info(f'Connected to server: {guild}')
-            for channel in guild.channels:
-                if 'annons' in channel.name and 'text' == channel.type.name:
-                    self.logger.info(f'Found channel for posting ads: {channel.name}')
-                    # await channel.send('Testing annons channel')
-                    await self.add_cog(DealsScraperCog(self, channel))
+            if guild.name in self.servers: # check if whitelisted server
+                self.logger.info(f'Connected to server: {guild}')
+                for channel in guild.channels:
+                    for c in self.channels:
+                        if c in channel.name and channel.type.name == 'text': # check if whitelisted channel
+                            self.logger.info(f'Found channel for posting deals: {channel.name}')
+                            # await channel.send('Successfully connected!')
+                            self.allowed_channels.append(channel)
+                            break
+        await self.add_cog(DealsScraperCog(self, self.allowed_channels))
 
 def main ():
-    intents = discord.Intents.default()
-    intents.message_content = True
-    handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
-    client = DealsBot(intents=None, command_prefix='!')    
-    client.run('', log_handler=handler, log_level=logging.INFO)
+    logger = logging.getLogger(LOGGER)
+    load_dotenv()
+    config = configparser.ConfigParser()
+    servers = []
+    channels = []
+    try:
+        config.read(CONFIG)
+        ss = config.get(CONFIG_DISCORD, CONFIG_DISCORD_SERVERS).split(',')
+        for s in ss:
+            servers.append(s.strip())
+        cc = config.get(CONFIG_DISCORD, CONFIG_DISCORD_CHANNELS).split(',')
+        for c in cc:
+            channels.append(c.strip())
+    except:
+        logger.info('Failed to parse config.ini')
+        exit(1)
+    handler = logging.FileHandler(filename=LOG_FILE, encoding='utf-8', mode='a')
+    client = DealsBot(servers, channels, logger, intents=None, command_prefix='!')    
+    client.run(os.getenv(TOKEN), log_handler=handler, log_level=logging.INFO)
 
 if __name__ == "__main__":
     main()
